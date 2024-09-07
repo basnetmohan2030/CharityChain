dc1a5b070f4adb14ba9e01871888249575dcf8f9
basnetmohan2030 <basnet.anish2030@gmail.com>
2024-09-07 15:39:11 +0545

    Landing Page Completion

--- charitychain/src/App.jsx
+++ charitychain/src/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 import LandingPage from './LandingPage'
-import './index.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-        <LandingPage />
+      <LandingPage />
     </>
   )
 }
